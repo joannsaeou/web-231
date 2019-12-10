a79ef9ee0ca7046ae8445e2f7094fc05bce6f17c
Joann Saeou <joannssaeou@gmail.com>
2019-12-09 21:07:21 -0600

    reply to Jeff Lintel

---
+++ week-2/reply-to-jeff-lintel.js
@@ -0,0 +1,5 @@
+/*
+The way you explain how functions works in Javascript is also understandable. After re-reading your discussion about the definition in javascript makes me realized and know what defining
+  javascript function really meant now. 
+
+  */
