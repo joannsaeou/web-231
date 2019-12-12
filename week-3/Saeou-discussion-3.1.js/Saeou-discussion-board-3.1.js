f69fdbd6db61002b794f848cfa2e98e8cc1172b5
Joann Saeou <joannssaeou@gmail.com>
2019-12-11 20:35:06 -0600

    adding dicussion board 3.1

---
+++ week-3/Saeou-discussion-3.1.js/Saeou-discussion-board-3.1.js
@@ -0,0 +1,20 @@
+/* 
+
+Title: Discussion Board 3.1
+; Author: Professor Krasso
+; Date: 25 June 2017
+; Modified By: Joann Saeou
+; Description: finding the two errors using -if else for while - functions property .
+
+*/
+
+var purses = ["Chanel", "Gucci", "Coach", "Michael Kors", "Hermes", "LV"];
+var text = "";
+var i;
+for (i = 6; i < purse.topchart; i++) {
+  text += "the top luxury purse number is" + i + "<br>";
+  
+}
+
+console.log( "purses" + 'text' + 'i' + '\n for'); 
+
