1415e6d22cd8b93d611cc91c019f2e444fa85ac4
Joann Saeou <joannssaeou@gmail.com>
2019-12-22 02:30:40 -0600

    adding files to week 4

--- week-4/starter-code/exercise-4.2.js
+++ week-4/starter-code/exercise-4.2.js
@@ -1,9 +1,49 @@
+
+const header = require('../../week-2/Saeou-header');
+
+
+console.log(header.display("Joann", "Saeou", "discussion 4.1  arrays"));
+console.log("\n");
+
+
+
+/*
+============================================
+; Title: Saeou-discussion-arrays-4.2.js
+; Author: Krasso  
+; Date: 17 December 2019
+; Modified By: Joann Saeou
+; Description: exercise 4.2 week 4 Javascript arrays
+;===========================================
+*/
+
+//creating variables with  function getFruits while logging into the console 
+var fruits = ["apple", "banana", "kiwi", "watermelon", "melon"];
+function getFruit(fruits) {
+  for(i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+  }
+   }
+   getFruit(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
   Expected output:
 
-  FirstName LastName
+  Joann Saeou
   Exercise 4.2
-  Today's Date
+  12/22/2019
 
   Apple
   Orange
--- week-4/starter-code/exercise-4.3.js
+++ week-4/starter-code/exercise-4.3.js
@@ -1,9 +1,71 @@
+const header = require('../../week-2/Saeou-header');
+
+
+console.log(header.display("Joann", "Saeou", "discussion 4.1  arrays"));
+console.log("\n");
+
+
+
+/*
+Title: Assignment 4.3
+; Author: Professor Krasso
+; Date: 18 December 2019
+; Modified By: Joann Saeou
+; Description: This program demonstrates the
+; use of JavaScript Arrays, filtering, and
+; and variables.
+*/
+
+//start program
+
+
+
+//adding variables to getValue of vehicles using the for, if statement
+
+var vehicles = ["bus", "van", "train", "coupe", "boat"];
+function getValue (vehicles, match) {
+  for(i = 0; i < vehicles.length; i++) {
+    if(match == vehicles[i]) {
+      console.log(match);
+    }
+
+  }
+}
+ 
+//an element to select certain vehicle 
+getValue(vehicles, "bus");
+
+
+
+
+//end of program
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
   Expected output:
 
-  FirstName LastName
+ Joann Saeou
   Exercise 4.3
-  Today's Date
+  12/21/2019
 
   -- DISPLAYING ARRAY ITEMS --
   Car
